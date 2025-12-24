c7ce3bcb76f7aca006011ba25f016195040cb16e
devops509 <devops@sector7hq.co>
2025-12-24 13:38:30 +0530

    initial commit from dev branch

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -95,7 +95,7 @@ function RecommendedProducts({
 }) {
   return (
     <div className="recommended-products">
-      <h2>Recommended Products</h2>
+      <h2>Dev Branch</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {(response) => (
