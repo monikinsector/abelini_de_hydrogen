1f619997beffc67404851e3632868dd4ee68a5c5
ganeshnadar-abl <ganesh.nadar@soulible.com>
2026-01-08 20:07:24 +0530

    [ABL-000][bug fix]:home page design change

--- app/components/Homepage/SpotlightSection.tsx
+++ app/components/Homepage/SpotlightSection.tsx
@@ -34,25 +34,25 @@ const Spotlight = () => {
 
   return (
 
-    <section className="lg:px-10 py-8 flex flex-col bg-[#FCF4EC] text-center overflow-x-hidden">
-      <div className="mb-4 ">
-        <p className="mt-3 text-p-14 font-light text-primary mb-2 tracking-wider">WHERE DO THEY MENTION US?</p>
+    <section className="bg-[#FCF4EC]">
+      <div className="container-fluid px-4 lg:px-8 py-12 mb-6 block">
+        <p className="mb-3 text-p-14 font-light text-primary tracking-wider text-center">WHERE DO THEY MENTION US?</p>
         <h2 className="flex justify-center capitalize m-0 text-h2 font-bold text-primary my-4 tracking-wider">Abelini In The Spotlight</h2>
-      </div>
-      <hr className="border-0 border-t border-solid border-primary my-6 mx-auto w-[100%]" />
-      <div className="flex flex-nowrap gap-10 items-center justify-around overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+        <hr className="border-0 border-t border-solid border-primary my-6 mx-auto w-[100%]" />
+        <div className="flex flex-nowrap gap-10 items-center justify-around overflow-x-scroll [&::-webkit-scrollbar]:hidden">
 
-        {logos.map((logo) => (
-          <Image
-            key={logo.id}
-            src={logo.image}
-            alt={logo.name}
-            width={170}
-            height={100}
-            loading="lazy"
-            className="w-[170px] h-auto object-contain"
-          />
-        ))}
+          {logos.map((logo) => (
+            <Image
+              key={logo.id}
+              src={logo.image}
+              alt={logo.name}
+              width={170}
+              height={100}
+              loading="lazy"
+              className="w-[170px] h-auto object-contain"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
