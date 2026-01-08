4b721aaa15d0e83e124cf4f9a648d604e984ffa1
darshanmundra <darshan.mundra@soulible.com>
2026-01-08 09:57:34 +0530

    worked on making the feature header responsive

--- app/components/Layout/FeatureHeader.tsx
+++ app/components/Layout/FeatureHeader.tsx
@@ -3,11 +3,13 @@ import { Image } from "@shopify/hydrogen";
 function FeatureHeader() {
     return (
       <div className="w-full bg-[#F3E7D7]">
-        <div className="mx-auto max-w-5xl">
+        <div className="mx-auto max-w-5xl px-4">
           <div className="w-full flex justify-between py-3">
-            <p className="text-[#343a40] text-[14px]">100% money back guarantee</p>
+            <p className="text-[#343a40] text-[14px] md:block hidden">100% money back guarantee</p>
+            <Image src="/assets/images/icons/phone.svg" alt="Phone" width={18} className="block md:hidden"/>
             <p className="text-[#343a40] text-[14px] flex items-center gap-2">Excellent 4.9 out of 5 <Image src="/assets/images/icons/trustpilot.svg" alt="Trustpilot" className="!w-4"/> Trustpilot</p>
-            <p className="text-[#343a40] text-[14px]">Fairly Priced Diamonds</p>
+            <Image src="/assets/images/icons/location.svg" alt="Location" width={18} className="block md:hidden"/>
+            <p className="text-[#343a40] text-[14px] md:block hidden">Fairly Priced Diamonds</p>
           </div>
         </div>
       </div>
