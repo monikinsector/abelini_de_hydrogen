da35f3fac7c1a2d8dddea9bcf255a0188f6e6ba9
ganeshnadar-abl <ganesh.nadar@soulible.com>
2026-01-12 12:53:58 +0530

    [ABL-52][feature]:footer mobile design fixed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -328,7 +328,7 @@ function FooterMenu({
                     to={link.href}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="underline lg:text-primary lg:text-p-14 text-p-16 lg:text-secondary text-quaternary font-medium"
+                    className="underline lg:text-primary lg:text-p-14 text-p-16 text-quaternary lg:font-regular font-medium"
                   >
                     {link.label}
                   </Link>
@@ -352,59 +352,4 @@ function FooterMenu({
       </div>
     </nav>
   );
-}
-
-const FALLBACK_FOOTER_MENU = {
-  id: 'gid://shopify/Menu/199655620664',
-  items: [
-    {
-      id: 'gid://shopify/MenuItem/461633060920',
-      resourceId: 'gid://shopify/ShopPolicy/23358046264',
-      tags: [],
-      title: 'Privacy Policy',
-      type: 'SHOP_POLICY',
-      url: '/policies/privacy-policy',
-      items: [],
-    },
-    {
-      id: 'gid://shopify/MenuItem/461633093688',
-      resourceId: 'gid://shopify/ShopPolicy/23358013496',
-      tags: [],
-      title: 'Refund Policy',
-      type: 'SHOP_POLICY',
-      url: '/policies/refund-policy',
-      items: [],
-    },
-    {
-      id: 'gid://shopify/MenuItem/461633126456',
-      resourceId: 'gid://shopify/ShopPolicy/23358111800',
-      tags: [],
-      title: 'Shipping Policy',
-      type: 'SHOP_POLICY',
-      url: '/policies/shipping-policy',
-      items: [],
-    },
-    {
-      id: 'gid://shopify/MenuItem/461633159224',
-      resourceId: 'gid://shopify/ShopPolicy/23358079032',
-      tags: [],
-      title: 'Terms of Service',
-      type: 'SHOP_POLICY',
-      url: '/policies/terms-of-service',
-      items: [],
-    },
-  ],
-};
-
-function activeLinkStyle({
-  isActive,
-  isPending,
-}: {
-  isActive: boolean;
-  isPending: boolean;
-}) {
-  return {
-    fontWeight: isActive ? 'bold' : undefined,
-    color: isPending ? 'grey' : 'white',
-  };
 }
