3f904b74e884b054974ba6e05bfa908130b64ba5
ganeshnadar-abl <ganesh.nadar@soulible.com>
2026-01-15 12:29:05 +0530

    [ABL-63][feature]:home page spotlight section

--- app/components/Homepage/SpotlightSection.tsx
+++ app/components/Homepage/SpotlightSection.tsx
@@ -12,7 +12,7 @@ const Spotlight = () => {
   return (
 
     <section className="bg-bg-primary">
-      <div className="container-fluid px-4 lg:px-10 my-10 block flex flex-col">
+      <div className="container-fluid px-4 lg:px-10 py-10 block flex flex-col">
         <p className="mb-3 text-p-14 font-light text-primary tracking-wider text-center">WHERE DO THEY MENTION US?</p>
         <h2 className="flex justify-center capitalize lg:text-h2 text-h2-m lg:leading-h2 leading-h2-m font-bold text-primary tracking-wider text-center mb-3">Abelini In The Spotlight</h2>
         <hr className="border-0 border-t border-solid border-primary my-6 mx-auto w-full" />
