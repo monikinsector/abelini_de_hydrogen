3b7088cd3410f94dea2fc2be66d769897f8c146f
ganeshnadar-abl <ganesh.nadar@soulible.com>
2026-01-17 12:37:00 +0530

    [ABL-59][bugfix]:data type added

--- tests/test-setup.ts
+++ tests/test-setup.ts
@@ -1,1 +1,23 @@
 import '@testing-library/jest-dom';
+import React from 'react';
+
+// Mock react-router Link component
+jest.mock('react-router', () => ({
+  Link: ({ to, children, className, id, ...props }: any) => (
+    React.createElement('a', { href: to, className, ...props }, children)
+  ),
+}));
+
+// Mock Shopify Hydrogen Image component
+jest.mock('@shopify/hydrogen', () => ({
+  Image: ({ src, alt, className, width, height, ...props }: any) => (
+    React.createElement('img', { 
+      src, 
+      alt, 
+      className, 
+      width, 
+      height, 
+      ...props 
+    })
+  ),
+}));
